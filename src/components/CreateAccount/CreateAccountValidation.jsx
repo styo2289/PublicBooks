64e39d6fd9e6fcc1fb04cd86716fffe4dd71f093
Summer <summertyo@yahoo.com>
2025-04-03 15:10:01 -0500

    designed 'create account' page with updated css and correct sign up validation.

--- src/components/CreateAccount/CreateAccountValidation.jsx
+++ src/components/CreateAccount/CreateAccountValidation.jsx
@@ -5,15 +5,58 @@ function CreateAccountValidation(firstName, lastName, userName, email, password)
         '^[a-zA-Z][a-zA-Z\\s]{0,20}[a-zA-Z]$'
     )
     const validUsername = new RegExp(
-        '^[a-zA-Z0-9_-]{4,16}$'
+        '^(?=.{4,16}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$'
     )
     const validEmail = new RegExp(
-        '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|.(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
+        '^(([^<>()[\\]\\\\.,;:\\s@"]+(\\.[^<>()[\\]\\\\.,;:\\s@"]+)*)|.(".+"))@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$'
     )
     const validPassword = new RegExp(
-        '^(?=.*?[A-Za-z])(?=.*?[0-9]).{6,}$'
+        '^.*(?=.{8,})(?=.*[a-zA-Z])(?=.*\\d)(?=.*[!#$%&? "]).*$'
     )
 
+    if(firstName === ''){
+        errors.firstName = "First name cannot be empty."
+    } else if(!validName.test(firstName)){
+        errors.firstName = "Not a valid first name."
+    } else {
+        errors.firstName = ""
+    }
+
+    if(lastName === ''){
+        errors.lastName = "Last name cannot be empty."
+    } else if(!validName.test(lastName)){
+        errors.lastName = "Not a valid last name."
+    } else {
+        errors.lastName = ""
+    }
+
+    if(userName === ''){
+        errors.userName = "Username cannot be empty."
+    } else if(userName.length < 4 || userName.length >= 16){
+        errors.userName = "Username must be minimum of 4 characters and maximum of 16 characters."
+    } else if(!validUsername.test(userName)){
+        errors.userName = "Not a valid username."
+    } else {
+        errors.userName = ""
+    }
+
+    if(email === ''){
+        errors.email = "Email cannot be empty."
+    } else if(!validEmail.test(email)){
+        errors.email = "Not a valid email."
+    } else {
+        errors.email = ""
+    }
+
+    if(password === ''){
+        errors.password = "Password cannot be empty."
+    } else if(!validPassword.test(password)){
+        errors.password = "Password must contain 8 characters and at least one number, one letter and one unique character."
+    } else {
+        errors.password = ""
+    }
+
+    return errors
 }
 
 export default CreateAccountValidation;
--- src/pages/CreateAccountPage.jsx
+++ src/pages/CreateAccountPage.jsx
@@ -2,6 +2,7 @@ import styles from './css-files/CreateAccountPage.module.css'
 import {useState} from "react";
 import axios from "axios";
 import {useNavigate} from "react-router-dom";
+import {Link} from "react-router-dom"
 import CreateAccountValidation from '../components/CreateAccount/CreateAccountValidation'
 
 function CreateAccountPage(){
@@ -13,6 +14,8 @@ function CreateAccountPage(){
         password: ''
     })
 
+    const [errors, setErrors] = useState({})
+
     // TODO: INSERT VALIDATION OF INFORMATION: regex validation for all fields
 
     const navigate = useNavigate()
@@ -20,52 +23,72 @@ function CreateAccountPage(){
     function handleSubmit(e){
         e.preventDefault()
 
-        axios.post('/add_user', values)
-            .then((res) => {
-                navigate('/')
-                console.log(res)
-            })
-            .catch((err) => console.log(err))
+        const err = CreateAccountValidation(values.firstName, values.lastName, values.userName, values.email, values.password)
+        setErrors(err)
+
+        if(errors.firstName === '' && errors.lastName === '' && errors.userName === '' && errors.email === '' && errors.password === ''){
+            axios.post('/add_user', values)
+                .then((res) => {
+                    navigate('/')
+                    console.log(res)
+                })
+                .catch((err) => console.log(err))
+        }
     }
     return(
-        <div className={styles.background}>
-            <div className={styles.wrapper}>
-                <form action="" onSubmit={handleSubmit}>
-                    <h1>Add Student</h1>
-                    <div className={styles.input}>
-                        <label className={styles.label}>First Name</label>
-                        <input type="text" name="firstName"
-                        onChange={(e)=> setValues({...values, firstName: e.target.value})}
-                        />
-                    </div>
-                    <div className={styles.input}>
-                        <label className={styles.label}>Last Name</label>
-                        <input type="text" name="lastName"
-                        onChange={(e)=> setValues({...values, lastName: e.target.value})}
-                        />
-                    </div>
-                    <div className={styles.input}>
-                        <label className={styles.label}>User Name</label>
-                        <input type="text" name="userName"
-                        onChange={(e) => setValues({...values, userName: e.target.value})}
-                        />
-                    </div>
-                    <div className={styles.input}>
-                        <label className={styles.label}>Email</label>
-                        <input type="text" name="email"
-                        onChange={(e)=> setValues({...values, email: e.target.value})}
-                        />
-                    </div>
-                    <div className={styles.input}>
-                        <label className={styles.label}>Password</label>
-                        <input type="password" name="password"
-                        onChange={(e) => setValues({...values, password: e.target.value})}
-                        />
-                    </div>
-                    <div className={styles.button}>
-                        <button type="submit">Submit</button>
-                    </div>
-                </form>
+        <div>
+            <div className={styles.background}>
+                <div className={styles.wrapper}>
+                    <form action="" onSubmit={handleSubmit}>
+                        <h1>Create Account</h1>
+                        <div className={styles.inputBox}>
+                            <label>First Name</label>
+                            <input type="text" name="firstName"
+                                   placeholder={"John"}
+                                   onChange={(e) => setValues({...values, firstName: e.target.value})}
+                            />
+                            {errors.firstName && <span className={styles.formErrors}>{errors.firstName}</span>}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <label>Last Name</label>
+                            <input type="text" name="lastName"
+                                   placeholder={"Smith"}
+                                   onChange={(e) => setValues({...values, lastName: e.target.value})}
+                            />
+                            {errors.lastName && <span className={styles.formErrors}>{errors.lastName}</span>}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <label>User Name</label>
+                            <input type="text" name="userName"
+                                   placeholder={"jsmith123"}
+                                   onChange={(e) => setValues({...values, userName: e.target.value})}
+                            />
+                            {errors.userName && <span className={styles.formErrors}>{errors.userName}</span>}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <label>Email</label>
+                            <input type="text" name="email"
+                                   placeholder={"JohnSmith@gmail.com"}
+                                   onChange={(e) => setValues({...values, email: e.target.value})}
+                            />
+                            {errors.email && <span className={styles.formErrors}>{errors.email}</span>}
+                        </div>
+                        <div className={styles.inputBox}>
+                            <label>Password</label>
+                            <input type="password" name="password"
+                                   placeholder={"Password"}
+                                   onChange={(e) => setValues({...values, password: e.target.value})}
+                            />
+                            {errors.password && <span className={styles.formErrors}>{errors.password}</span>}
+                        </div>
+                        <div>
+                            <button type="submit">Submit</button>
+                        </div>
+                        <div className={styles.haveAccount}>
+                            <p>Have an account? Click <Link to="/loginPage">here</Link> to login.</p>
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>
     );
